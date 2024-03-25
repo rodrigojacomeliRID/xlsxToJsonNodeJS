fc68b085c1e1f19f43c4232980e562579e21e772
Rodrigo Jacomeli RID <rodrigo.jacomeli.redeinova@gmail.com>
2024-03-25 13:50:21 -0300

    Add formating data to string's

--- index.js
+++ index.js
@@ -2,10 +2,25 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var xlsx = require("xlsx");
 var fs = require("fs");
-var filePath = './pbm.xlsx';
-var jsonFilePath = './pbm.json';
+var filePath = "./pbm.xlsx";
+var jsonFilePath = "./pbm.json";
 var file = xlsx.readFile(filePath);
 var worksheet = file.Sheets[file.SheetNames[0]];
 var jsonData = xlsx.utils.sheet_to_json(worksheet);
-fs.writeFileSync(jsonFilePath, JSON.stringify(jsonData));
-console.log('Convert Successfully!!!');
+var jsonConvertDataString = [];
+jsonData.forEach(function (i) {
+    jsonConvertDataString.push({
+        ean: i.ean.toString(),
+        medicamento: i.medicamento,
+        industria: i.industria,
+        plataforma: i.plataforma,
+        desc_paciente: i.desc_paciente.toString(),
+        obs_desconto: i.obs_desconto || "",
+        programa: i.programa,
+        tel_cadastro: i.tel_cadastro,
+        link_cadastro: i.link_cadastro,
+    });
+});
+console.log(jsonConvertDataString);
+fs.writeFileSync(jsonFilePath, JSON.stringify(jsonConvertDataString));
+console.log("Convert Successfully!!!");
--- index.ts
+++ index.ts
@@ -1,14 +1,44 @@
-import * as xlsx from 'xlsx';
-import * as fs from 'fs';
+import * as xlsx from "xlsx";
+import * as fs from "fs";
 
-const filePath = './pbm.xlsx';
-const jsonFilePath = './pbm.json';
+interface PBM {
+  ean: string | number;
+  medicamento: string;
+  industria: string;
+  plataforma: string;
+  desc_paciente: string | number;
+  obs_desconto?: string | null;
+  programa: string;
+  tel_cadastro: string;
+  link_cadastro: string;
+}
+
+const filePath = "./pbm.xlsx";
+const jsonFilePath = "./pbm.json";
 
 const file = xlsx.readFile(filePath);
 
 const worksheet = file.Sheets[file.SheetNames[0]];
-const jsonData = xlsx.utils.sheet_to_json(worksheet);
+const jsonData: PBM[] = xlsx.utils.sheet_to_json(worksheet);
 
-fs.writeFileSync(jsonFilePath, JSON.stringify(jsonData));
+const jsonConvertDataString: PBM[] = [];
 
-console.log('Convert Successfully!!!');
+jsonData.forEach((i: PBM) => {
+  jsonConvertDataString.push({
+    ean: i.ean.toString(),
+    medicamento: i.medicamento,
+    industria: i.industria,
+    plataforma: i.plataforma,
+    desc_paciente: i.desc_paciente.toString(),
+    obs_desconto: i.obs_desconto || "",
+    programa: i.programa,
+    tel_cadastro: i.tel_cadastro,
+    link_cadastro: i.link_cadastro,
+  });
+});
+
+console.log(jsonConvertDataString);
+
+fs.writeFileSync(jsonFilePath, JSON.stringify(jsonConvertDataString));
+
+console.log("Convert Successfully!!!");
